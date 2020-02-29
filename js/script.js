65cd02fb1bb015d49e1782842012d03365ac4c09
Raul Mercado <raumercadox@gmail.com>
2020-02-29 18:11:50 -0500

    Se inclyen monedas

--- js/script.js
+++ js/script.js
@@ -5,19 +5,26 @@ billetes.push(new Billete(50, 4));
 billetes.push(new Billete(20, 5));
 billetes.push(new Billete(200, 4));
 billetes.push(new Billete(100, 5));
+billetes.push(new Billete(5, 20));
+billetes.push(new Billete(1, 15));
+
+var billetes = billetes.sort((a, b) => b.denominacion - a.denominacion);
 
 var cajero = new Cajero(billetes);
 var respuesta = document.getElementById("respuesta");
 var btnRetirar = document.getElementById("btnRetirar");
+mostrarSaldo();
+
 btnRetirar.addEventListener("click", function(e){
     var importe = document.getElementById("txtImporte").value;
     var efectivo = recibirDinero(importe);
-    respuesta.innerHTML = efectivo.length == 0 ? "No hay cash suficiente" : "";
+    respuesta.innerHTML = efectivo.length == 0 ? "No se puede entregar esa cantidad de dinero" : "";
     for (var b of efectivo)
     {
         if (b.cantidad > 0)
         {
             imprimirBilletes(b);
+            mostrarSaldo();
         }
     }
 })
@@ -25,8 +32,8 @@ btnRetirar.addEventListener("click", function(e){
 function recibirDinero(importe)
 {
     var efectivo = [];
-    var billetesOrdenados = billetes.sort((a, b) => b.denominacion - a.denominacion);
-    for (var b of billetesOrdenados)
+    
+    for (var b of billetes)
     {
         var numBilletes = parseInt(importe / b.denominacion) ;
         if (numBilletes > b.cantidad)
@@ -38,11 +45,21 @@ function recibirDinero(importe)
     }
     if (importe == 0)
     {
+        debitarCajero(efectivo);
         return efectivo;
     }
     else
     {
         return [];
+    }
+}
+
+function debitarCajero(efectivo)
+{
+    for (var b of efectivo)
+    {
+        var billete = billetes.find((elemento, indice, objeto) => elemento.denominacion == b.denominacion);
+        billete.cantidad -= b.cantidad;
     }
 }
 
@@ -52,7 +69,24 @@ function imprimirBilletes(billete)
     {
         var imagenBillete = new Image();
         imagenBillete.src = "img/" + billete.denominacion + ".jpg";
+        if (billete.denominacion < 10)
+        {
+            imagenBillete.width = 70;
+            imagenBillete.height = 70;
+        }
         respuesta.appendChild(imagenBillete);
     }
     respuesta.appendChild(document.createElement("br"));
+}
+
+function mostrarSaldo()
+{
+    var saldo = document.getElementById("saldo");
+    var reporteSaldo = "<table><tr><td><strong>Denominacion</strong></td><td><strong>Cantidad</strong></td></tr>";
+    for(var b of billetes)
+    {
+        reporteSaldo += "<tr><td>" + b.denominacion + " Soles</td><td>" + b.cantidad + "</td></tr>";
+    }
+    reporteSaldo += "</table>";
+    saldo.innerHTML = reporteSaldo;
 }
